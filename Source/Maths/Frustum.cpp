d8e12dcf5bba02a048ef2ffbd75432f481cd6ffe
Kirill Trofimov <trofkm@gmail.com>
2024-02-07 15:36:49 +0300

    fix: Use enum class

--- Source/Maths/Frustum.cpp
+++ Source/Maths/Frustum.cpp
@@ -2,8 +2,8 @@
 
 #include "../Physics/AABB.h"
 
-enum Planes {
-    Near,
+enum class Planes : size_t {
+    Near = 0,
     Far,
     Left,
     Right,
@@ -21,40 +21,40 @@ float Plane::distanceToPoint(const glm::vec3 &point) const
 void ViewFrustum::update(const glm::mat4 &mat) noexcept
 {
     // left
-    m_planes[Planes::Left].normal.x = mat[0][3] + mat[0][0];
-    m_planes[Planes::Left].normal.y = mat[1][3] + mat[1][0];
-    m_planes[Planes::Left].normal.z = mat[2][3] + mat[2][0];
-    m_planes[Planes::Left].distanceToOrigin = mat[3][3] + mat[3][0];
+    m_planes[(size_t)Planes::Left].normal.x = mat[0][3] + mat[0][0];
+    m_planes[(size_t)Planes::Left].normal.y = mat[1][3] + mat[1][0];
+    m_planes[(size_t)Planes::Left].normal.z = mat[2][3] + mat[2][0];
+    m_planes[(size_t)Planes::Left].distanceToOrigin = mat[3][3] + mat[3][0];
 
     // right
-    m_planes[Planes::Right].normal.x = mat[0][3] - mat[0][0];
-    m_planes[Planes::Right].normal.y = mat[1][3] - mat[1][0];
-    m_planes[Planes::Right].normal.z = mat[2][3] - mat[2][0];
-    m_planes[Planes::Right].distanceToOrigin = mat[3][3] - mat[3][0];
+    m_planes[(size_t)Planes::Right].normal.x = mat[0][3] - mat[0][0];
+    m_planes[(size_t)Planes::Right].normal.y = mat[1][3] - mat[1][0];
+    m_planes[(size_t)Planes::Right].normal.z = mat[2][3] - mat[2][0];
+    m_planes[(size_t)Planes::Right].distanceToOrigin = mat[3][3] - mat[3][0];
 
     // bottom
-    m_planes[Planes::Bottom].normal.x = mat[0][3] + mat[0][1];
-    m_planes[Planes::Bottom].normal.y = mat[1][3] + mat[1][1];
-    m_planes[Planes::Bottom].normal.z = mat[2][3] + mat[2][1];
-    m_planes[Planes::Bottom].distanceToOrigin = mat[3][3] + mat[3][1];
+    m_planes[(size_t)Planes::Bottom].normal.x = mat[0][3] + mat[0][1];
+    m_planes[(size_t)Planes::Bottom].normal.y = mat[1][3] + mat[1][1];
+    m_planes[(size_t)Planes::Bottom].normal.z = mat[2][3] + mat[2][1];
+    m_planes[(size_t)Planes::Bottom].distanceToOrigin = mat[3][3] + mat[3][1];
 
     // top
-    m_planes[Planes::Top].normal.x = mat[0][3] - mat[0][1];
-    m_planes[Planes::Top].normal.y = mat[1][3] - mat[1][1];
-    m_planes[Planes::Top].normal.z = mat[2][3] - mat[2][1];
-    m_planes[Planes::Top].distanceToOrigin = mat[3][3] - mat[3][1];
+    m_planes[(size_t)Planes::Top].normal.x = mat[0][3] - mat[0][1];
+    m_planes[(size_t)Planes::Top].normal.y = mat[1][3] - mat[1][1];
+    m_planes[(size_t)Planes::Top].normal.z = mat[2][3] - mat[2][1];
+    m_planes[(size_t)Planes::Top].distanceToOrigin = mat[3][3] - mat[3][1];
 
     // near
-    m_planes[Planes::Near].normal.x = mat[0][3] + mat[0][2];
-    m_planes[Planes::Near].normal.y = mat[1][3] + mat[1][2];
-    m_planes[Planes::Near].normal.z = mat[2][3] + mat[2][2];
-    m_planes[Planes::Near].distanceToOrigin = mat[3][3] + mat[3][2];
+    m_planes[(size_t)Planes::Near].normal.x = mat[0][3] + mat[0][2];
+    m_planes[(size_t)Planes::Near].normal.y = mat[1][3] + mat[1][2];
+    m_planes[(size_t)Planes::Near].normal.z = mat[2][3] + mat[2][2];
+    m_planes[(size_t)Planes::Near].distanceToOrigin = mat[3][3] + mat[3][2];
 
     // far
-    m_planes[Planes::Far].normal.x = mat[0][3] - mat[0][2];
-    m_planes[Planes::Far].normal.y = mat[1][3] - mat[1][2];
-    m_planes[Planes::Far].normal.z = mat[2][3] - mat[2][2];
-    m_planes[Planes::Far].distanceToOrigin = mat[3][3] - mat[3][2];
+    m_planes[(size_t)Planes::Far].normal.x = mat[0][3] - mat[0][2];
+    m_planes[(size_t)Planes::Far].normal.y = mat[1][3] - mat[1][2];
+    m_planes[(size_t)Planes::Far].normal.z = mat[2][3] - mat[2][2];
+    m_planes[(size_t)Planes::Far].distanceToOrigin = mat[3][3] - mat[3][2];
 
     for (auto &plane : m_planes) {
         float length = glm::length(plane.normal);
