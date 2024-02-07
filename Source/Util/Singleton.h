339d06ac15a6022d0f205b12845e821fe1b929b7
Kirill Trofimov <trofkm@gmail.com>
2024-02-07 15:38:34 +0300

    feat: Add templated singleton

--- Source/Util/Singleton.h
+++ Source/Util/Singleton.h
@@ -4,7 +4,16 @@
 #include "NonCopyable.h"
 #include "NonMovable.h"
 
-class Singleton : public NonMovable, public NonCopyable {
+class Singleton : public NonMovable, public NonCopyable {};
+
+template <typename T> class TSingleton : public NonCopyable, public NonMovable {
+  public:
+    // in golang you can implement such structure using sync.Once
+    static T &instance()
+    {
+        static T s_instance;
+        return s_instance;
+    }
 };
 
 #endif // SINGLETON_H_INCLUDED
